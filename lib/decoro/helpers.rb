027a2c5f4de1f7a0880b45d586ee27e17e01540d
Mark Cornelissen <mark@boxture.com>
2020-06-25 14:39:54 +0200

    Handle the scenario where you get a single instance

--- lib/decoro/helpers.rb
+++ lib/decoro/helpers.rb
@@ -5,12 +5,15 @@ module Decoro
     def decorate(model, decorator_class = nil)
       array = nil
       klass = decorator_class
+
       if model.is_a? Array
         array = model
         model = model.first
         klass ||= "#{model.class}Decorator".constantize
       elsif model.is_a? ActiveRecord::Relation
         klass ||= "#{model.name.pluralize}Decorator".constantize
+      else
+        klass ||= "#{model.class}Decorator".constantize
       end
 
       decorator = array ? array.map { |m| klass.new(m) } : klass.new(model)
